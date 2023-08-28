cb0eb8eac74729d6f4ad1099828a369ac704f0cb
Basimov Rustam <sombra1nf@yandex.ru>
2023-08-28 18:32:35 +0500

    Удалил лишний импорт

--- src/main/java/faang/school/achievement/achievementHandler/EventHandler.java
+++ src/main/java/faang/school/achievement/achievementHandler/EventHandler.java
@@ -1,7 +1,5 @@
 package faang.school.achievement.achievementHandler;
 
-import faang.school.achievement.dto.invitation.StageInvitationEvent;
-
 public interface EventHandler<T> {
 
     void process(T event);
