fd54f086d25f83fe19df4a4541bf03633bbf82c8
Sergey Gerasimov <czar997@yandex.ru>
2023-08-30 19:19:37 +0300

    1. Десериализация json вынесена в абстрактный класс 2. Исправлена названия полей в yaml

--- src/main/java/faang/school/achievement/listener/InviteEventListener.java
+++ src/main/java/faang/school/achievement/listener/InviteEventListener.java
@@ -2,31 +2,27 @@ package faang.school.achievement.listener;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import faang.school.achievement.dto.InviteSentEventDto;
-import faang.school.achievement.exception.DeserializeJSONException;
 import faang.school.achievement.hundler.EventHandler;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.connection.Message;
 import org.springframework.data.redis.connection.MessageListener;
 import org.springframework.stereotype.Component;
 
-import java.io.IOException;
 import java.util.List;
 
 @Component
-@RequiredArgsConstructor
-public class InviteEventListener implements MessageListener {
-
-    private final ObjectMapper objectMapper;
+public class InviteEventListener extends AbstractEventListener<InviteSentEventDto> implements MessageListener {
     private final List<EventHandler> eventHandlers;
+
+    @Autowired
+    public InviteEventListener(ObjectMapper objectMapper, List<EventHandler> eventHandlers) {
+        super(objectMapper);
+        this.eventHandlers = eventHandlers;
+    }
 
     @Override
     public void onMessage(Message message, byte[] pattern) {
-        InviteSentEventDto inviteSentEventDto;
-        try {
-            inviteSentEventDto = objectMapper.readValue(message.getBody(), InviteSentEventDto.class);
-        } catch (IOException e) {
-            throw new DeserializeJSONException("Unable to deserialize invite sent event");
-        }
+        InviteSentEventDto inviteSentEventDto = convertJsonToString(message, InviteSentEventDto.class);
         eventHandlers.forEach(handler -> handler.handle(inviteSentEventDto));
     }
 }
