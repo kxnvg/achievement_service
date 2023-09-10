acbba1770a9277f0803ace9c92725fb5e9baf55b
Dregid <ufl852@gmail.com>
2023-09-10 16:25:56 +0600

    handler half-already

--- src/main/java/faang/school/achievement/handler/EventHandler.java
+++ src/main/java/faang/school/achievement/handler/EventHandler.java
@@ -1,5 +1,7 @@
 package faang.school.achievement.handler;
 
+import faang.school.achievement.service.AchievementService;
+
 public interface EventHandler<T> {
     boolean handle(T event);
 }
--- src/main/java/faang/school/achievement/handler/SenseyAchievementHandler.java
+++ src/main/java/faang/school/achievement/handler/SenseyAchievementHandler.java
@@ -1,13 +1,36 @@
 package faang.school.achievement.handler;
 
+import faang.school.achievement.cache.AchievementCache;
+import faang.school.achievement.dto.AchievementDto;
 import faang.school.achievement.dto.redis.MentorshipStartEvent;
+import faang.school.achievement.mapper.AchievementMapper;
+import faang.school.achievement.model.Achievement;
+import faang.school.achievement.service.AchievementService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class SenseyAchievementHandler implements EventHandler<MentorshipStartEvent> {
+    private static final String ACHIEVEMENT_NAME = "Сенсей";
+    private final AchievementService achievementService;
+    private final AchievementCache achievementCache;
+    private final AchievementMapper achievementMapper;
+
     @Override
     public boolean handle(MentorshipStartEvent event) {
+        AchievementDto achievementDto = getAchievementDto();
+        if (hasAchievement(event, achievementDto)) {
 
+        }
         return false;
+    }
+
+    private boolean hasAchievement(MentorshipStartEvent event, AchievementDto achievementDto) {
+        return !achievementService.hasAchievement(event.getMentorId(), achievementDto.getId());
+    }
+
+    private AchievementDto getAchievementDto() {
+        return achievementMapper.toDto(achievementCache.get(ACHIEVEMENT_NAME));
     }
 }
