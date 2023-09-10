e08ed9998c63c46315bbc29b1701a94dba54169c
RasulMurtuzaliev <ras8705@mail.ru>
2023-09-10 11:22:36 +0500

    добавил аннотацию на маппер

--- src/main/java/faang/school/achievement/mapper/AchievementProgressMapper.java
+++ src/main/java/faang/school/achievement/mapper/AchievementProgressMapper.java
@@ -2,8 +2,11 @@ package faang.school.achievement.mapper;
 
 import faang.school.achievement.dto.AchievementProgressDto;
 import faang.school.achievement.model.AchievementProgress;
+import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.ReportingPolicy;
 
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface AchievementProgressMapper {
     @Mapping(source = "achievement.id", target = "achievementId")
     AchievementProgressDto toDto(AchievementProgress achievementProgress);
