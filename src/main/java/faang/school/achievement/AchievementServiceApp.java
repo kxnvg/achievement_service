afd1f4c5591205d4be944e5b0a278124ef0e9a1c
RasulMurtuzaliev <ras8705@mail.ru>
2023-09-12 20:04:25 +0500

    обновил RedisConfig

--- src/main/java/faang/school/achievement/AchievementServiceApp.java
+++ src/main/java/faang/school/achievement/AchievementServiceApp.java
@@ -1,7 +1,6 @@
 package faang.school.achievement;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import faang.school.achievement.model.Achievement;
 import org.springframework.boot.Banner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -9,9 +8,6 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableAsync;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @SpringBootApplication
 @EnableFeignClients("faang.school.achievement.client")
