7dd354fe0a467e65cd1b770227d7897af533ec54
adil <adil.200191@gmail.com>
2023-09-16 18:05:30 +0600

    Создал листенера и хэндлер ProfilePicEvent для достижения Красвачик

--- src/main/java/faang/school/achievement/config/redis/RedisConfiguration.java
+++ src/main/java/faang/school/achievement/config/redis/RedisConfiguration.java
@@ -2,6 +2,7 @@ package faang.school.achievement.config.redis;
 
 import com.sun.source.util.TaskEvent;
 import faang.school.achievement.listener.GoalSetListener;
+import faang.school.achievement.listener.ProfilePicListener;
 import faang.school.achievement.listener.TaskEventListener;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
@@ -20,10 +21,13 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 public class RedisConfiguration {
     private final GoalSetListener goalSetListener;
     private final TaskEventListener taskEventListener;
+    private final ProfilePicListener profilePicListener;
     @Value("${spring.data.redis.channels.goal_set_channel.name}")
     private String goalSetChannel;
     @Value("${spring.data.redis.channels.task_channel.name}")
     private String taskChannel;
+    @Value("${spring.data.redis.channels.profile_pic_channel.name}")
+    private String profilePicChannel;
     @Value("${spring.data.redis.host}")
     private String host;
     @Value("${spring.data.redis.port}")
@@ -50,6 +54,7 @@ public class RedisConfiguration {
         container.setConnectionFactory(connectionFactory);
         container.addMessageListener(goalSetListener, new ChannelTopic(goalSetChannel));
         container.addMessageListener(taskEventListener, new ChannelTopic(taskChannel));
+        container.addMessageListener(profilePicListener, new ChannelTopic(profilePicChannel));
         return container;
     }
 }
