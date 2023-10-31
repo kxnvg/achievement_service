c99736e0675f3886518beb5c6db40ea1e0f06841
Stepan Palkov <step.palkov@gmail.com>
2023-11-01 02:08:00 +0300

    merge

--- src/main/java/faang/school/achievement/config/RedisConfig.java
+++ src/main/java/faang/school/achievement/config/RedisConfig.java
@@ -10,14 +10,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 @RequiredArgsConstructor
@@ -77,7 +74,9 @@ public class RedisConfig {
     @Bean
     RedisMessageListenerContainer redisContainer(MessageListenerAdapter recommendationEventAdapter,
                                                  MessageListenerAdapter inviteEventAdapter,
-                                                 MessageListenerAdapter skillAcquiredEventAdapter) {
+                                                 MessageListenerAdapter skillAcquiredEventAdapter,
+                                                 MessageListenerAdapter postEventAdapter,
+                                                 MessageListenerAdapter commentEventAdapter) {
         RedisMessageListenerContainer container = new RedisMessageListenerContainer();
         container.setConnectionFactory(redisConnectionFactory());
         container.addMessageListener(recommendationEventAdapter, topicRecommendation());
